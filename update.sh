c8d8e95756a384355ca5d69b3a767b3f66e485cb
Tom Adams <tom@dxw.com>
2013-07-10 15:56:55 -0400

    Oops

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -xe
 
+rm -rf roots-copy
 git submodule update --init --recursive
 cp -r roots roots-copy
-find roots-copy -name .git -type d -print0 | xargs -0 rm -rf
+find roots-copy -name .git -print0 | xargs -0 rm -rf
