985161a9ef23f8df7c13e3ff6267cf64fc47b552
Tom Adams <tom@dxw.com>
2013-08-05 15:26:17 -0400

    Update roots

---
+++ Makefile
@@ -0,0 +1,5 @@
+copy:
+	rm -rf roots-copy
+	git submodule update --init --recursive
+	cp -r roots roots-copy
+	find roots-copy -name .git -print0 | xargs -0 rm -rf
--- roots-copy/lib/scripts.php
+++ roots-copy/lib/scripts.php
@@ -4,32 +4,24 @@
  *
  * Enqueue stylesheets in the following order:
  * 1. /theme/assets/css/bootstrap.css
- * 2. /theme/assets/css/bootstrap-responsive.css
- * 3. /theme/assets/css/app.css
- * 4. /child-theme/style.css (if a child theme is activated)
+ * 2. /theme/assets/css/app.css
  *
  * Enqueue scripts in the following order:
- * 1. jquery-1.10.1.min.js via Google CDN
+ * 1. jquery-1.10.2.min.js via Google CDN
  * 2. /theme/assets/js/vendor/modernizr-2.6.2.min.js
  * 3. /theme/assets/js/plugins.js (in footer)
  * 4. /theme/assets/js/main.js    (in footer)
  */
 function roots_scripts() {
   wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
-  wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
-
-  // Load style.css from child theme
-  if (is_child_theme()) {
-    wp_enqueue_style('roots_child', get_stylesheet_uri(), false, null);
-  }
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
   // It's kept in the header instead of footer to avoid conflicts with plugins.
   if (!is_admin() && current_theme_supports('jquery-cdn')) {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js', false, null, false);
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, null, false);
     add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
   }
 
@@ -52,7 +44,7 @@ function roots_jquery_local_fallback($src, $handle) {
   static $add_jquery_fallback = false;
 
   if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery-1.10.1.min.js"><\/script>\')</script>' . "\n";
+    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery-1.10.2.min.js"><\/script>\')</script>' . "\n";
     $add_jquery_fallback = false;
   }
 
